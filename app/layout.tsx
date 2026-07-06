083d70dad29283af70a0fd197efa0a9a27edddee
Nikhil Singhal <singhalnikhil010@gmail.com>
2026-07-06 11:02:10 +0530

    fix symbols

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&amp;display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:FILL,wght@0..1,100..700&display=swap" rel="stylesheet"/>
       </head>
       <body className="min-h-full flex flex-col bg-background text-on-surface">
         <GlobalProvider>
