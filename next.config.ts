8147d095a0d3ff66a0c2b7f234eb0ad6f4080182
Nikhil Singhal <singhalnikhil010@gmail.com>
2026-07-17 01:37:17 +0530

    fix: Update Permissions-Policy in next.config.ts to allow camera, microphone, and display-capture

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()',
+            value: 'camera=(self), microphone=(self), display-capture=(self)',
           },
           {
             key: 'X-XSS-Protection',
